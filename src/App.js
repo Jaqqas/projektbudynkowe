d28dce860aa0641db48d728ceb46bbbde511cabc
Jaqqas <kubak1332@gmail.com>
2022-04-25 02:16:35 +0200

    ver 0.2

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
-import React from "react";
+import React, {useState} from "react";
 import "./App.css";
-
+import {Axios} from "axios";
 
 function App() {
+
+    const[loginReg, setLoginReg] = useState("");
+    const[passwordReg, setPasswordReg] = useState("");
+
+    const register = () => {
+        Axios.post('http://localhost:3001/register',
+            {login: loginReg,
+            password: passwordReg,
+            }).then((Response) =>{
+                console.log(Response);
+        })
+    };
+
   return (
     <div className="App">
       <div className="Kontener">
@@ -19,10 +32,24 @@ function App() {
         <div className="register">
             <h2>Rejestracja</h2>
             <label>Login:</label>
-            <input type="text"  name="login" placeholder="login..."/>
+            <input
+                type="text"
+                name="login"
+                placeholder="login..."
+                onChange={(e) =>{
+                    setLoginReg(e.target.value);
+                }}
+            />
             <label>Haslo:</label>
-            <input type="password"  name="password" placeholder="haslo..."/>
-            <button>Zarejestruj</button>
+            <input
+                type="password"
+                name="password"
+                placeholder="haslo..."
+                onChange={(e) => {
+                    setPasswordReg(e.target.value);
+                }}
+            />
+            <button onClick={register}>Zarejestruj</button>
         </div>
       </div>
     </div>
